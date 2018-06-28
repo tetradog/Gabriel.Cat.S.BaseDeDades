cda53c30c4d5fc5e1732efaff07a563bab794042
gabriel <tetradog@gmail.com>
2018-06-28 06:58:25 +0200

    subir datos iniciales

---
+++ Gabriel.Cat.S.BaseDeDades/Atributos/Name.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gabriel.Cat.S.BaseDeDades
+{
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Property)]
+    /// <summary>
+    /// Nombres de tablas y campos
+    /// </summary>
+    /// 
+    public class Name:System.Attribute
+    {
+
+        string name;
+        public Name(string name)
+        {
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
---
+++ Gabriel.Cat.S.BaseDeDades/BaseDeDatos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gabriel.Cat.S.BaseDeDades
+{
+    public abstract class BaseDeDatos
+    {
+    }
+}
