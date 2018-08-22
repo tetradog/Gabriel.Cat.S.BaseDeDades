98ab78f794f3d620d48ccd691de7ea98cf4152e1
gabriel <tetradog@gmail.com>
2018-08-22 14:26:52 +0200

    ahora cuando se elimine un objeto se podrá decidir que hacer.

---
+++ Gabriel.Cat.S.BaseDeDades/Atributos/Remove.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gabriel.Cat.S.BaseDeDades
+{
+    [AttributeUsage(System.AttributeTargets.Property)]
+    public class RemoveSQL:System.Attribute
+    {
+        public enum Option
+        {
+            Null,
+            Default,
+            Delete
+        }
+
+
+        public Option OptionOnDelete { get; private set; }
+
+        public RemoveSQL(Option optionOnDelete=Option.Null)
+        {
+            OptionOnDelete = optionOnDelete;
+        }
+        public override string ToString()
+        {
+            return OptionOnDelete.ToString();
+        }
+    }
+}
