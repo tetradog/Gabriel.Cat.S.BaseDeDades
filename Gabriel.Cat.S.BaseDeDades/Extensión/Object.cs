3057d21466304ef12a42bee3f51f2ebf7cf597ac
tetradog <tetradog@gmail.com>
2019-05-01 18:57:48 +0200

    simplificando el codigo

--- Gabriel.Cat.S.BaseDeDades/Extensión/Object.cs
+++ Gabriel.Cat.S.BaseDeDades/Extensión/Object.cs
@@ -19,15 +19,14 @@ namespace Gabriel.Cat.S.Extension
         {
             List<Utilitats.Propiedad> propiedades=obj.GetPropiedades();
             List<KeyValuePair<string, object>> propiedadesSQL = new List<KeyValuePair<string, object>>();
-            NameSQL nameSQL;
-            string name;
             //ordeno
             List<OrderSQL> ordenSQLs = new List<OrderSQL>();
             SortedList<OrderSQL,Utilitats.Propiedad> propiedadesConOrden=new SortedList<OrderSQL, Utilitats.Propiedad>();
             OrderSQL orden;
+
             for (int i=propiedades.Count-1;i>=0;i--)
             {
-                orden = propiedades[i].Info.Atributos.Filtra((atr) => atr is OrderSQL).FirstOrDefault() as OrderSQL;
+                orden = propiedades[i].TryGetOrder();
                 if(orden!=null)
                 {
                     ordenSQLs.Add(orden);
@@ -45,7 +44,7 @@ namespace Gabriel.Cat.S.Extension
             for(int i=0;i<propiedades.Count;i++)
             {
                 //miro si tiene el atributo ignore
-                if (PropiedadSerializable(propiedades[i])&&propiedades[i].Info.Atributos.OfType<IgnoreSQL>().Count() == 0)
+                if (propiedades[i].IsPropertySQL())
                 {
                     propiedadesSQL.Add(new KeyValuePair<string, object>(propiedades[i].GetNameSQL(), propiedades[i].Objeto));
                 }
@@ -54,15 +53,7 @@ namespace Gabriel.Cat.S.Extension
             return propiedadesSQL;
         }
        
-        private static bool PropiedadSerializable(Propiedad propiedad)
-        {
-            bool esSerializable = propiedad.Info.Uso==(UsoPropiedad.Get|UsoPropiedad.Set);
-            if (!esSerializable && propiedad.Info.Tipo.ImplementInterficie(typeof(IList<>)))
-                esSerializable = true;
 
-            
-            return esSerializable;
-        }
 
         public static string CreateTableSQL(this object obj,BaseDeDatos bd)
         {
--- Gabriel.Cat.S.BaseDeDades/Extensión/Propiedad.cs
+++ Gabriel.Cat.S.BaseDeDades/Extensión/Propiedad.cs
@@ -1,4 +1,8 @@
+using Gabriel.Cat.S.Extension;
 using Gabriel.Cat.S.Utilitats;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Gabriel.Cat.S.BaseDeDades
 {
 	public static class Propiedad
@@ -11,6 +15,23 @@ namespace Gabriel.Cat.S.BaseDeDades
                 name = nameSQL.ToString();
             else name = propiedad.Info.Nombre;
             return name;
+        }
+        private static bool PropiedadSerializable(this Utilitats.Propiedad propiedad)
+        {
+            bool esSerializable = propiedad.Info.Uso == (UsoPropiedad.Get | UsoPropiedad.Set);
+            if (!esSerializable && propiedad.Info.Tipo.ImplementInterficie(typeof(IList<>)))
+                esSerializable = true;
+
+
+            return esSerializable;
+        }
+        public static bool IsPropertySQL(this Utilitats.Propiedad propiedad)
+        {
+            return PropiedadSerializable(propiedad) && propiedad.Info.Atributos.OfType<IgnoreSQL>().Count() == 0;
+        }
+        public static OrderSQL TryGetOrder(this Utilitats.Propiedad propiedad)
+        {
+            return propiedad.Info.Atributos.Filtra((atr) => atr is OrderSQL).FirstOrDefault() as OrderSQL;
         }
     }
 }
